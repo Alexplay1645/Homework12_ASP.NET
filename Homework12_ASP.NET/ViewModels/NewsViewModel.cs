cdb7180dcac37b31c2b87bca4f932b696a4129b9
Alexplay1645 <149514571+Alexplay1645@users.noreply.github.com>
2025-09-06 18:45:14 -0700

    Update NewsViewModel.cs

--- Homework12_ASP.NET/ViewModels/NewsViewModel.cs
+++ Homework12_ASP.NET/ViewModels/NewsViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace Homework12_ASP.NET.ViewModels
+namespace YourProjectName.ViewModels
 {
     public class NewsViewModel
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string ShortContent { get; set; } = string.Empty;
+
+        public string PublishedDate { get; set; } = string.Empty;
     }
 }
